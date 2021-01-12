efe43e17bb56f0d889eb315a4615007a91c1bcbb
Julie Desvaux <desvaux.julie@gmail.com>
2021-01-12 22:32:18 +0100

    First test React_Native with hooks

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import axios from 'axios';
+import Svg, { Image } from 'react-native-svg';
 
-export default function App() {
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const  SvgComponent = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Svg height="50%" width="50%" viewBox="0 0 100 100" {...props}>
+    </Svg>
+  );
+}
+
+const App = () => {
+
+  const [countries, setCountries] = useState(null)
+
+  useEffect(() => {
+    if (countries === null) {
+      async function fetchData() {
+        await axios.get('http://restcountries.eu/rest/v2/all')
+          .then(response => setCountries(response.data))
+      }
+      fetchData()
+    }
+  })
+
+  const renderItem = ({ item }) => (
+      <View style={styles.country}>
+        <Svg>
+          <Image href={`${item.flag}`} style={styles.image}/>
+        </Svg>
+          {/* <Image 
+            source={{ uri: `${item.flag}` }} 
+            style={styles.image}
+          /> */}
+          <Text>
+              {item.name}
+          </Text>
+          <Text>
+              {item.capital}
+          </Text>
+      </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={countries}
+        renderItem={renderItem}
+        keyExtractor={countries => countries.alpha3Code}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+      paddingTop: 30,
+      fontSize: 50,
+      textDecorationLine: "underline",
+      textAlign: "center"
+  },
+  country: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+  },
+  image: {
+      flex: 1,
+      width: 150,
+      height: 100,
+      resizeMode: 'contain'
+  }
 });
+
+export default App;
